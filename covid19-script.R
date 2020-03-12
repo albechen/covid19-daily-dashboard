e71847b70d32b2596b857a6c085815acf805ecfe
albechen <alchen244@gmail.com>
2020-03-11 22:08:59 -0400

    chore: auto-update 2020-03-11 22:08:59 est

--- covid19-script.R
+++ covid19-script.R
@@ -1,5 +1,5 @@
 library(rmarkdown)
 Sys.setenv(RSTUDIO_PANDOC="C:\Users\alche\Program Files\RStudio\bin\pandoc")
 setwd("C:\Users\alche\Documents\Projects\covid19-daily-dashboard")
-rmarkdown::render("covid19-daily-dashboard.Rmd", include=TRUE)
+rmarkdown::render("covid19-daily-dashboard.Rmd")
 
